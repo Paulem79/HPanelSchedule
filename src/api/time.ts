a7ddc9163f591f9cb21fa371c71cfb360c999aff
Paulem <66211574+Paulem79@users.noreply.github.com>
2025-09-21 14:03:18 +0200

    feat(time): faster time api

--- src/api/time.ts
+++ src/api/time.ts
@@ -3,25 +3,25 @@ import {getIp} from "./ip.ts";
 interface TimeApiResponse {
     year: number;
     month: number;
+    monthName: string;
     day: number;
-    hour: number;
-    minute: number;
+    dayofweek: number;
+    dayofweekName: string;
+    hours: number;
+    minutes: number;
     seconds: number;
-    milliSeconds: number;
-    dateTime: string;
-    date: string;
-    time: string;
-    timeZone: string;
-    dayOfWeek: string;
-    dstActive: boolean;
+    millis: number;
+    fulldate: string;
+    timezone: string;
+    status: string;
 }
 
 export async function getCurrentTime() {
     const res = await getAPIResponse();
-    return new Date(res.dateTime);
+    return new Date(res.fulldate);
 }
 
 async function getAPIResponse(): Promise<TimeApiResponse> {
-    const res = await fetch(`https://timeapi.io/api/time/current/ip?ipAddress=${await getIp()}`);
+    const res = await fetch(`https://script.google.com/macros/s/AKfycbyd5AcbAnWi2Yn0xhFRbyzS4qMq1VucMVgVvhul5XqS9HkAyJY/exec?tz=Europe/Paris`);
     return await res.json();
 }
